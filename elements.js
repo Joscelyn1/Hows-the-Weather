283e34d41fdb99d1a69100b616b9af74624eefd6
Joscelyn <JoscelynRose@gmail.com>
2018-10-04 20:42:47 -0700

    added some code to select DOM elements

---
+++ elements.js
@@ -0,0 +1,9 @@
+export const ELEMENT_SEARCH_BUTTON = document.querySelector('#showWeatherButton');
+export const ELEMENT_SEARCHED_CITY = document.querySELECTOR('#city');
+
+export const ELEMENT_LOADING_TEXT = document.querySELECTOR('#load');
+export const ELEMENT_WEATHER_BOX = document.querySELECTOR('#weather');
+
+export const ELEMENT_WEATHER_CITY = ELEMENT_WEATHER_BOX.firstElementChild;
+export const ELEMENT_WEATHER_DESCRIPTION = document.querySElector('#weatherDescription');
+export const ELEMENT_WEATHER_TEMPERATURE = ELEMENT_WEATHER_BOX.lastElementChild;
